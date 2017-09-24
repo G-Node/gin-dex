f6808e3b0805d3b86347d14493419142780e19cd
cgars <christian@stuebeweg50.de>
2017-09-25 08:13:00 +0900

    Logging when repo indexing

--- gindex/repo.go
+++ gindex/repo.go
@@ -5,24 +5,41 @@ import (
 	log "github.com/Sirupsen/logrus"
 )
 
-func IndexRepoWithPath(path string, serv *ElServer) error {
+func IndexRepoWithPath(path string, serv *ElServer, repoid string) error {
+	log.Info("Start indexing a repository with path:%s", path)
 	rep, err := gig.OpenRepository(path)
 	if err != nil {
 		return err
 	}
-	commits, err := rep.WalkRef("master", func(comitID gig.SHA1) bool {
+	log.Info("Did open repo")
+	commits, err := rep.WalkRef("tag1", func(comitID gig.SHA1) bool {
 		res, err := serv.HasCommit("commits", comitID)
 		if err != nil {
 			log.Printf("Could not querry commit index: %v", err)
 			return false
 		}
-		return res
+		return !res
 	})
+	log.Infof("Found n commits: %d", len(commits))
 	if err != nil {
 		return err
 	}
-	for _, commit := range commits{
-		NewCommitFromGig(commit).AddToIndex(serv, "commits")
+	for _, commit := range commits {
+		err := NewCommitFromGig(commit, repoid).AddToIndex(serv, "commits")
+		blobs := make(map[gig.SHA1]*gig.Blob)
+		rep.GetBlobsForCommit(commit, blobs)
+		for blid, blob := range blobs {
+			hasBlob, err := serv.HasBlob("blobs", blid)
+			if err != nil {
+				return err
+			}
+			if !hasBlob {
+				NewBlobFromGig(blob, repoid).AddToIndex(serv, "blobs")
+			}
+		}
+		if err != nil {
+			log.Printf("Big problem2")
+		}
 	}
 	return nil
 }
