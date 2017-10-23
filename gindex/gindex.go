66b88df3b7edcec029410b7e915a49c56c65512a
cgars <christian@stuebeweg50.de>
2017-10-23 11:13:53 +0200

    Search and indexing  structs added

--- gindex/gindex.go
+++ gindex/gindex.go
@@ -2,10 +2,10 @@ package gindex
 
 type SearchRequest struct {
 	Token  string
+	CsrfT  string
 	UserID int
 	Querry string
 }
-
 
 type IndexRequest struct {
 	Token    string
@@ -16,4 +16,19 @@ type IndexRequest struct {
 type GinServer struct {
 	URL     string
 	GetRepo string
+}
+
+type BlobSResult struct {
+	Source *IndexBlob `json:"_source"`
+	Score  float64    `json:"_score"`
+}
+
+type CommitSResult struct {
+	Source *IndexCommit `json:"_source"`
+	Score  float64    `json:"_score"`
+}
+
+type SearchResults struct {
+	Blobs []BlobSResult
+	Commits []CommitSResult
 }
